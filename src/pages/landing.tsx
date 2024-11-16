600e2d5d4b87f4214869ecfb2be92d1996d0f08c
Aakash_074 <aakash@getsafle.com>
2024-11-17 04:18:48 +0700

    fix: update

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from 'react';
 import { useDynamicContext, DynamicWidget } from '@dynamic-labs/sdk-react-core'
 import { useNavigate } from 'react-router-dom';
+import BuckLogo from '../assets/buckit.jpg'
+import RetroGrid from '@/components/ui/retro-grid';
+import ShimmerButton from '@/components/ui/shimmer-button';
 
 //@ts-ignore
 export const Home = () => {
@@ -46,8 +49,18 @@ export const Home = () => {
 //   }
 
   return (
-    <div style={{display: 'flex', flexDirection: 'row', height: '100vh', width: '100vw'}}>
-        <DynamicWidget />
+    <div style={{display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw'}} className='justify-center items-center relative'>
+        <img src={BuckLogo} alt="buck" className='max-w-80' />
+        <div className='text-xl font-bold'>BUCKIT</div>
+        <div className='text-md'>Bucketlist of Travel Inspiration</div>
+        <div className='w-full justify-center'>
+        <DynamicWidget innerButtonComponent={<ShimmerButton className="shadow-2xl">
+        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+          Login / Signup
+        </span>
+      </ShimmerButton>} />
+        </div>
+        <RetroGrid />
     </div>
   );
 };
